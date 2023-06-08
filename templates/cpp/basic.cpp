aa63554a684b323afa7f5ca590f7d02d23c7258f
Lucas <propilideno@gmail.com>
2023-06-08 13:20:49 -0300

    improving basic format, and removing useless things for basic template

--- templates/cpp/basic.cpp
+++ templates/cpp/basic.cpp
@@ -1,21 +1,21 @@
 // ############################ Template available in: https://propi.dev/cp  ############################ //
-
 #include <bits/stdc++.h>
-
+//Debug methods
 #define _(x) {cout << #x << " = " << x << endl;} //Print with endl
 #define __(x) {cout << #x << " = " << x << " | ";} //Print without endl
 #define _vec(x) {cout << #x << " = "; for(int i : x){cout << i << " ";} cout << endl;} //Print entire vector
-#define _pair(x) {cout << #x << " = | " << "1st: " << x.first << " | " << "2nd: " << x.second << endl;} //Print pair
-
-const double E = 1e-8;
+//Constants
 const double PI = acos(-1);
-
+//Types
 using namespace std;
-
+typedef long long ll;
+typedef unsigned long long ull;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
 // ###################################################################################################### //
 
 int main(){
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false); //If false, don't use stdio and iostream at the same time
 	string line;
 	while(getline(cin,line)){
 		cout << line << endl;
