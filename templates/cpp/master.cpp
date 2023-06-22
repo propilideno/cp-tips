9964542c47b79616315770a7eeddd84c85f07537
lucas <lucas@propi.dev>
2023-06-22 10:47:20 -0300

    improved master.cpp structure and time output

--- templates/cpp/master.cpp
+++ templates/cpp/master.cpp
@@ -6,7 +6,9 @@
 #define __(x) {cout << #x << " = " << x << " | ";} //Print without endl
 #define _vec(x) {cout << #x << " = "; for(int i : x){cout << i << " ";} cout << endl;} //Print entire vector
 #define _pair(x) {cout << #x << " = | " << "1st: " << x.first << " | " << "2nd: " << x.second << endl;} //Print pair
-#define __time__ { auto duration = chrono::duration<double>( std::chrono::high_resolution_clock::now() - beg); _(duration.count()); }
+#define __time__ { auto duration = chrono::duration<double>( \
+std::chrono::high_resolution_clock::now() - beg);\
+cout<<"Time: "<<duration.count()<<endl;}
 //Constants
 const auto beg = std::chrono::high_resolution_clock::now(); //Begining of the program
 const double PI = acos(-1); //PI
@@ -21,19 +23,7 @@ const int MOD = 1e9+7; // Util with mod operations (prime number)
 #define mp make_pair
 #define ff first
 #define ss second
-//loops
-#define f(i,n) for(int i=0;i<n;i++) //From 0 to n-1
-#define rf(i,n) for(int i=n-1;i>=0;i--) //From n-1 to 0
-#define F(i,a,b) for(int i=a;i<b;i++)
-#define RF(i,a,b) for(int i=a;i>=b;i--)
-#define FOR(it,c) for ( __typeof((c).begin()) it=(c).begin(); it!=(c).end(); it++ )
-#define RFOR(it,c) for ( __typeof((c).rbegin()) it=(c).rbegin(); it!=(c).rend(); it++ )
-#define REP(it,c) for ( __typeof((c).begin()) it=(c).begin(); it!=(c).end();)
-#define all(c) c.begin(), c.end()
-#define rall(c) c.rbegin(), c.rend()
-//IO Optimization
-#define SpeedUP ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-//Data Structures and Types
+//Shortened Types
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
@@ -46,16 +36,22 @@ typedef std::map<int, int> mii;
 typedef std::map<ll, ll> mll;
 typedef std::pair<int, int> pii;
 typedef std::pair<std::string, int> psi;
+//loops
+#define f(i,n) for(int i=0;i<n;i++) //From 0 to n-1
+#define rf(i,n) for(int i=n-1;i>=0;i--) //From n-1 to 0
+#define F(i,a,b) for(int i=a;i<b;i++)
+#define RF(i,a,b) for(int i=a;i>=b;i--)
+#define FOR(it,c) for ( __typeof((c).begin()) it=(c).begin(); it!=(c).end(); it++ )
+#define RFOR(it,c) for ( __typeof((c).rbegin()) it=(c).rbegin(); it!=(c).rend(); it++ )
+#define REP(it,c) for ( __typeof((c).begin()) it=(c).begin(); it!=(c).end();)
+#define all(c) c.begin(), c.end()
+#define rall(c) c.rbegin(), c.rend()
+//IO Optimization
+#define SpeedUP ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 // Custom Data Structs
 struct Graph {
-    int n; vector<vector<int>> adj;
-
-    Graph(int n) : n(n) { adj.resize(n); }
-
-    void addEdge(int u, int v) {
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
+    int n; vector<vector<int>> adj; Graph(int n) : n(n) { adj.resize(n); }
+    void addEdge(int u, int v) { adj[u].push_back(v); adj[v].push_back(u); }
 };
 // HEADERS
 template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>; //Min Heap
@@ -96,7 +92,7 @@ void dbfs(Graph& G, int v, vector<bool>& visited) {
                 visited[w] = true;
             }
         }
-    }
+    } cout << endl;
 }
 
 /* ########################## Template available in: https://propi.dev/cp  ########################## */
