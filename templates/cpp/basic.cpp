56727043ff614d72e728c98533a2a57b7ae64765
lucas <lucas@propi.dev>
2023-07-25 18:11:02 -0300

    added time to basic.cpp

--- templates/cpp/basic.cpp
+++ templates/cpp/basic.cpp
@@ -4,11 +4,14 @@
 #define _(x) {cout << #x << " = " << x << endl;} //Print with endl
 #define __(x) {cout << #x << " = " << x << " | ";} //Print without endl
 #define _vec(x) {cout << #x << " = "; for(int i : x){cout << i << " ";} cout << endl;} //Print entire vector
-#define __log__ { std::FILE* file = std::freopen("LOG.txt", "w", stdout); }
+#define __log__ { std::FILE* file = std::freopen("LOG.txt", "w", stdout); } //Redirect output to LOG.txt
+#define __time__ { auto duration = chrono::duration<double>( /* Show runtime */ \
+std::chrono::high_resolution_clock::now() - beg); cout<<"Time: "<<duration.count()<<endl;}
 //Constants
+const auto beg = std::chrono::high_resolution_clock::now(); //Begining of the program
 const double PI = acos(-1);
 //IO Optimization
-#define __FasterIO__ ios_base::sync_with_stdio(false);//Don't use stdio and iostream at the same time
+#define __FasterIO__ ios_base::sync_with_stdio(false); //Don't use stdio and iostream at the same time
 //Types
 using namespace std;
 typedef long long ll;
@@ -25,4 +28,5 @@ int main(){
 		cout << line << endl;
 	}
 
+	//__time__			//Uncomment for show runtime
 }
